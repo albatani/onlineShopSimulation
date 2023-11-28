19c791009a29d18cd672bb75d5fbcf5fed9628d5
albatani <152284151+albatani@users.noreply.github.com>
2023-11-28 21:41:50 +0700

    Update classprojek.cpp

--- classprojek.cpp
+++ classprojek.cpp
@@ -21,7 +21,10 @@ void predefinedProduct()
 {
     productList[productCount++] = {"ASUS TUF F15 FX506HC", "ASUS", "Laptop", 1199.99, 115};
     productList[productCount++] = {"Lenovo LOQ 15 IRH-8", "LENOVO", "Laptop", 1399.90, 105};
-    productList[productCount++] = {"Resistor 1k", "Resistor", "Komponen", 500.00, 115};
+    productList[productCount++] = {"Nokia 106 Dual Sim", "NOKIA", "Handphone", 399.90, 201};
+    productList[productCount++] = {"HUAWEI Band 8", "HUAWEI", "Smartwatch", 599.90, 345};
+    productList[productCount++] = {"Kingston SSD A400 480GB", "KINGSTON", "SSD", 499.90, 401};
+    productList[productCount++] = {"Resistor 1k", "RESISTOR", "Komponen", 500.00, 115};
     //...
 }
 
@@ -39,9 +42,78 @@ void displayProducts()
     cout << endl;
 }
 
-void displayByCategory(int Typecategory)
+void displayByCategory(int typeCategory)
 {
-    // ---
+    if(typeCategory>0 && typeCategory<=5)
+    {
+        cout << "Products in the selected category : " << endl;
+        for (int i = 0; i < productCount; i++)
+        {
+            if (typeCategory == 1 && productList[i].productCategory == "Laptop")
+            {
+                cout << "Name : " << productList[i].productName;
+                cout << ", Brand : " << productList[i].productBrand;
+                cout << ", Category : " << productList[i].productCategory;
+                cout << ", Price : $" << productList[i].productPrice;
+                cout << ", ID : " << productList[i].productID << endl;
+            }
+            else if (typeCategory == 2 && productList[i].productCategory == "Handphone")
+            {
+                cout << "Name : " << productList[i].productName;
+                cout << ", Brand : " << productList[i].productBrand;
+                cout << ", Category : " << productList[i].productCategory;
+                cout << ", Price : $" << productList[i].productPrice;
+                cout << ", ID : " << productList[i].productID << endl;
+            }
+            else if (typeCategory == 3 && productList[i].productCategory == "Smartwatch")
+            {
+                cout << "Name : " << productList[i].productName;
+                cout << ", Brand : " << productList[i].productBrand;
+                cout << ", Category : " << productList[i].productCategory;
+                cout << ", Price : $" << productList[i].productPrice;
+                cout << ", ID : " << productList[i].productID << endl;
+            }
+            else if (typeCategory == 4 && productList[i].productCategory == "SSD")
+            {
+                cout << "Name : " << productList[i].productName;
+                cout << ", Brand : " << productList[i].productBrand;
+                cout << ", Category : " << productList[i].productCategory;
+                cout << ", Price : $" << productList[i].productPrice;
+                cout << ", ID : " << productList[i].productID << endl;
+            }
+            else if (typeCategory == 5 && productList[i].productCategory == "Komponen")
+            {
+                cout << "Name : " << productList[i].productName;
+                cout << ", Brand : " << productList[i].productBrand;
+                cout << ", Category : " << productList[i].productCategory;
+                cout << ", Price : $" << productList[i].productPrice;
+                cout << ", ID : " << productList[i].productID << endl;
+            }
+            
+        }
+    }
+     else
+    {
+            cout<<"ENTER THE AVAILABLE CATEGORIES!!!!!!!!!";
+    }
+    cout << endl;
+}
+
+void displayByBrand(string brand)
+{
+    cout << "Products by the selected brand : " << endl;
+    for (int i = 0; i < productCount; i++)
+    {
+        if (productList[i].productBrand == brand)
+        {
+            cout << "Name : " << productList[i].productName;
+            cout << ", Brand : " << productList[i].productBrand;
+            cout << ", Category : " << productList[i].productCategory;
+            cout << ", Price : $" << productList[i].productPrice;
+            cout << ", ID : " << productList[i].productID << endl;
+        }
+    }
+    cout << endl;
 }
 
 void displayByPrice() {
@@ -137,9 +209,22 @@ int main()
             displayProducts();
             break;
         case 2:
-            break;
-        case 3:
-            break;
+        {
+        int categoryOption;
+        cout << "Enter category option: " << endl;
+        cout << "1. Laptop\n2. Handphone\n3. Smartwatch\n4. SSD\n5. Komponen" << endl; 
+        cin >> categoryOption;
+        displayByCategory(categoryOption);
+        break;
+        }
+      case 3:
+        {
+        string brand;
+        cout << "Enter brand: ";
+        cin >> brand;
+        displayByBrand(brand);
+        break;
+        }
         case 4:
             displayByPrice();
             break;
